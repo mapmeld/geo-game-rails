4d7a997abce5b604191c233e8206690f499ec4e1
Derek Eder <derek.eder@gmail.com>
2013-04-30 11:18:07 -0500

    content and markup updates with Brooke

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -23,4 +23,13 @@ class HomeController < ApplicationController
     redirect_to '/'
   end
 
+  def start
+    @microbes = ApplicationHelper.microbes
+
+    @recent_photos = InstagramPhoto.select("instagram_photos.*, instagram_users.username, microbes.tag")
+                                   .joins("JOIN instagram_users on instagram_users.id = instagram_photos.instagram_user_id")
+                                   .joins("JOIN microbes on microbes.id = instagram_photos.microbe_id")
+                                   .limit(3).order("created_time DESC").all
+  end
+
 end
